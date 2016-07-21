56855a83149d746a06db0032c08f74b119d0d887
Michael Rispoli <M@Michaels-Air.home>
2016-07-21 07:51:24 -0400

    adding professional experience and contact form

--- _site/js/behave.js
+++ _site/js/behave.js
@@ -9,18 +9,6 @@ $(document).ready(function(){
   };
   var startTimer = setInterval(toggleNext, 6500);
 
-  // scroll zoom effect
-  // var prevScrollTop = 0;
-  // $(window).scroll(function() {
-  //   var currScrollTop = $(this).scrollTop();
-  //   if (currScrollTop > prevScrollTop) {
-  //     $('.slide').css('width', '-=10');
-  //   } else {
-  //     $('.slide').css('width', '+=10');
-  //   }
-  //
-  //   prevScrollTop = currScrollTop;
-  // })
   function unstick(el) {
     el.removeClass('sticky');
     el.next('.section').css('margin-top', '0');
@@ -42,9 +30,9 @@ $(document).ready(function(){
         unstick($this);
       }
   }
-
+  // sticky nav
   $(window).scroll(stickyAction);
-
+  //scrolling animation
   $('a[href^="#"]').click(function(event) {
     var target = $($(this).attr('href'));
     if( target.length ) {
@@ -54,4 +42,67 @@ $(document).ready(function(){
         }, 1000);
     }
   });
+  //contact form script modal fire
+  $('#contact-popup').modal({show:false})
+  //email address validator
+  function isValidEmail(email, e){
+  	   var is_email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  	   return is_email.test(email);
+  };
+  // post request for contact form
+  function contactSubmit(message) {
+  	$.ajax({
+  	    url: "https://formspree.io/mrispoli24@gmail.com",
+  	    method: "POST",
+  	    data: message,
+  	    dataType: "json"
+  	}).done(function(){
+  		$('#contact-form').hide();
+  		$('#success-message').show();
+  	});
+  };
+  //contact form event handler
+  $('#contact-form').on('submit',function(e){
+  	var data = {
+  		name: $('#name').val(),
+  		_replyto: $('#replyTo').val(),
+  		subject: $('#subject').val(),
+  		message: $('#message').val()
+  	};
+  	// check honeypot for bots
+  	if (!$('#gotcha').val().length) {
+  		// error handling
+  		if (!data.name.length) {
+  			$('#name-error').show();
+  			$('#name-error').parent().addClass('has-error');
+  		} else if (!isValidEmail(data._replyto)) {
+  			$('#name-error').hide();
+  			$('#name-error').parent().removeClass('has-error');
+  			$('#email-error').show();
+  			$('#email-error').parent().addClass('has-error');
+  		} else if (!data.subject.length) {
+  			$('#name-error').hide();
+  			$('#name-error').parent().removeClass('has-error');
+  			$('#email-error').hide();
+  			$('#email-error').parent().removeClass('has-error');
+  			$('#subject-error').show();
+  			$('#subject-error').parent().addClass('has-error');
+  		} else if (!data.message.length) {
+  			$('#name-error').hide();
+  			$('#name-error').parent().removeClass('has-error');
+  			$('#email-error').hide();
+  			$('#email-error').parent().removeClass('has-error');
+  			$('#subject-error').hide();
+  			$('#subject-error').parent().removeClass('has-error');
+  			$('#message-error').show();
+  			$('#message-error').parent().addClass('has-error');
+  		} else {
+  			contactSubmit(data);
+  		}
+  	}
+  	e.preventDefault();
+  });
+
+
+
 });
