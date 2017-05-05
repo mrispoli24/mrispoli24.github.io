f8f5ad2481b795ccd05a7f5ee844c3881bbfcc82
Mike Rispoli <mrispoli24@gmail.com>
2017-05-05 09:03:21 -0400

    adding in contact form and setting up resume

--- js/behave_2017.js
+++ js/behave_2017.js
@@ -14,5 +14,71 @@ $(document).ready(function(){
   });
   // end scrolling animation
 
+  // extended resume toggle
+  $('.js-extended-resume-toggle').click(function(event) {
+    event.preventDefault();
+    $('.js-extended-resume').toggle('active');
+  });
+
+  //email address validator
+  function isValidEmail(email, e){
+  	   var is_email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  	   return is_email.test(email);
+  };
+  // post request for contact form
+  function contactSubmit(message) {
+  	$.ajax({
+  	    url: "https://formspree.io/mrispoli24@gmail.com",
+  	    method: "POST",
+  	    data: message,
+  	    dataType: "json"
+  	}).done(function(){
+  		$('#contact-form').hide();
+  		$('#success-message').show();
+  	});
+  };
+
+  //contact form event handler
+  $('#contact-form').on('submit',function(e){
+  	var data = {
+  		name: $('#name').val(),
+  		_replyto: $('#replyTo').val(),
+  		subject: $('#subject').val(),
+  		message: $('#message').val()
+  	};
+  	// check honeypot for bots
+  	if (!$('#gotcha').val().length) {
+  		// error handling
+  		if (!data.name.length) {
+  			$('#name-error').show();
+  			$('#name-error').parent().addClass('has-error');
+  		} else if (!isValidEmail(data._replyto)) {
+  			$('#name-error').hide();
+  			$('#name-error').parent().removeClass('has-error');
+  			$('#email-error').show();
+  			$('#email-error').parent().addClass('has-error');
+  		} else if (!data.subject.length) {
+  			$('#name-error').hide();
+  			$('#name-error').parent().removeClass('has-error');
+  			$('#email-error').hide();
+  			$('#email-error').parent().removeClass('has-error');
+  			$('#subject-error').show();
+  			$('#subject-error').parent().addClass('has-error');
+  		} else if (!data.message.length) {
+  			$('#name-error').hide();
+  			$('#name-error').parent().removeClass('has-error');
+  			$('#email-error').hide();
+  			$('#email-error').parent().removeClass('has-error');
+  			$('#subject-error').hide();
+  			$('#subject-error').parent().removeClass('has-error');
+  			$('#message-error').show();
+  			$('#message-error').parent().addClass('has-error');
+  		} else {
+  			contactSubmit(data);
+  		}
+  	}
+  	e.preventDefault();
+  });
+
 });
 // end document ready
